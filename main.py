2a90eb14cdf9994d53739a7e892013af991418ec
Akshay Ashok <aks28id@gmail.com>
2020-09-07 11:44:38 +0530

    print added

--- main.py
+++ main.py
@@ -25,11 +25,13 @@ if __name__ == "__main__":
     for message in init.consumer_obj:
         message = message.value
         db_key = str(message)
+        print(db_key, 'db_key')
         db_object = Cache.objects.get(pk=db_key)
         file_name = db_object.file_name
         init.redis_obj.set(globals.RECEIVE_TOPIC, file_name)
         if db_object.is_doc_type:
             """document"""
+            print('in doc type')
             images_array = []
             for image in db_object.files:
                 pdf_image = str(uuid.uuid4()) + ".jpg"
@@ -59,12 +61,14 @@ if __name__ == "__main__":
                 "captions": captions_list,
                 "is_doc_type": True
             }
+            print(full_res, "full_res")
             send_to_topic(globals.SEND_TOPIC_FULL, value_to_send_dic=full_res)
             send_to_topic(globals.SEND_TOPIC_TEXT, value_to_send_dic=text_res)
             init.producer_obj.flush()
 
         else:
             """image"""
+            print('in image type')
             if db_object.mime_type in globals.ALLOWED_IMAGE_TYPES:
                 with open(file_name, 'wb') as file_to_save:
                     file_to_save.write(db_object.file.read())
@@ -76,6 +80,7 @@ if __name__ == "__main__":
                     "captions": full_res["captions"],
                     "is_doc_type": False
                 }
+                print(full_res, 'full_res')
                 send_to_topic(globals.SEND_TOPIC_FULL, value_to_send_dic=full_res)
                 send_to_topic(globals.SEND_TOPIC_TEXT, value_to_send_dic=text_res)
                 init.producer_obj.flush()
